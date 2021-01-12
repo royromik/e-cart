c9f0f22913eee9581a86ae468806c8c0ef4ffd0f
royromik <royromik@gmail.com>
2021-01-12 19:57:47 +0530

    fixed minor bug

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+      <h1>React App</h1>
+      </main>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Footer = () => {
+    return ( 
+        <h1>footer</h1>
+     );
+}
+ 
+export default Footer;
